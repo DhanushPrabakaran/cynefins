ee5ecee8fc04d8c7c5a17775ef3b03cb9e2688e2
DhanushPrabakaran <dhanushprabakaran18@gmail.com>
2023-10-18 06:56:20 +0530

    push

--- app/page.tsx
+++ app/page.tsx
@@ -32,12 +32,9 @@ const page = () => {
       <div className="hero min-h-screen bg-base-200">
         <div className="hero-content text-center">
           <div className="">
-            <h1 className="text-5xl font-bold p-2 ">
-              Team mates
-            </h1>
+            <h1 className="text-5xl font-bold p-2 ">Team mates</h1>
             {/*  */}
             <div className="flex flex-wrap">
-             
               {/*  */}
               <div className="card w-96 bg-base-100 shadow-xl m-1 ">
                 <figure className="px-10 pt-10">
@@ -58,9 +55,139 @@ const page = () => {
                 </div>
               </div>
               {/*  */}
-
-              
-              
+              <div className="card w-96 bg-base-100 shadow-xl m-1 ">
+                <figure className="px-10 pt-10">
+                  <Image
+                    src="https://img.freepik.com/free-vector/paper-style-dynamic-lines-background_23-2149008629.jpg"
+                    alt="Shoes"
+                    className="rounded-xl"
+                    width={400}
+                    height={400}
+                  />
+                </figure>
+                <div className="card-body items-center text-center">
+                  <h2 className="card-title">Name</h2>
+                  <p>desc</p>
+                  <div className="card-actions">
+                    <button className="btn btn-primary">Resume</button>
+                  </div>
+                </div>
+              </div>
+              {/*  */}
+              <div className="card w-96 bg-base-100 shadow-xl m-1 ">
+                <figure className="px-10 pt-10">
+                  <Image
+                    src="https://img.freepik.com/free-vector/paper-style-dynamic-lines-background_23-2149008629.jpg"
+                    alt="Shoes"
+                    className="rounded-xl"
+                    width={400}
+                    height={400}
+                  />
+                </figure>
+                <div className="card-body items-center text-center">
+                  <h2 className="card-title">Name</h2>
+                  <p>desc</p>
+                  <div className="card-actions">
+                    <button className="btn btn-primary">Resume</button>
+                  </div>
+                </div>
+              </div>
+              {/*  */}
+              <div className="card w-96 bg-base-100 shadow-xl m-1 ">
+                <figure className="px-10 pt-10">
+                  <Image
+                    src="https://img.freepik.com/free-vector/paper-style-dynamic-lines-background_23-2149008629.jpg"
+                    alt="Shoes"
+                    className="rounded-xl"
+                    width={400}
+                    height={400}
+                  />
+                </figure>
+                <div className="card-body items-center text-center">
+                  <h2 className="card-title">Name</h2>
+                  <p>desc</p>
+                  <div className="card-actions">
+                    <button className="btn btn-primary">Resume</button>
+                  </div>
+                </div>
+              </div>
+              {/*  */}
+              <div className="card w-96 bg-base-100 shadow-xl m-1 ">
+                <figure className="px-10 pt-10">
+                  <Image
+                    src="https://img.freepik.com/free-vector/paper-style-dynamic-lines-background_23-2149008629.jpg"
+                    alt="Shoes"
+                    className="rounded-xl"
+                    width={400}
+                    height={400}
+                  />
+                </figure>
+                <div className="card-body items-center text-center">
+                  <h2 className="card-title">Name</h2>
+                  <p>desc</p>
+                  <div className="card-actions">
+                    <button className="btn btn-primary">Resume</button>
+                  </div>
+                </div>
+              </div>
+              {/*  */}
+              <div className="card w-96 bg-base-100 shadow-xl m-1 ">
+                <figure className="px-10 pt-10">
+                  <Image
+                    src="https://img.freepik.com/free-vector/paper-style-dynamic-lines-background_23-2149008629.jpg"
+                    alt="Shoes"
+                    className="rounded-xl"
+                    width={400}
+                    height={400}
+                  />
+                </figure>
+                <div className="card-body items-center text-center">
+                  <h2 className="card-title">Name</h2>
+                  <p>desc</p>
+                  <div className="card-actions">
+                    <button className="btn btn-primary">Resume</button>
+                  </div>
+                </div>
+              </div>
+              {/*  */}
+              <div className="card w-96 bg-base-100 shadow-xl m-1 ">
+                <figure className="px-10 pt-10">
+                  <Image
+                    src="https://img.freepik.com/free-vector/paper-style-dynamic-lines-background_23-2149008629.jpg"
+                    alt="Shoes"
+                    className="rounded-xl"
+                    width={400}
+                    height={400}
+                  />
+                </figure>
+                <div className="card-body items-center text-center">
+                  <h2 className="card-title">Name</h2>
+                  <p>desc</p>
+                  <div className="card-actions">
+                    <button className="btn btn-primary">Resume</button>
+                  </div>
+                </div>
+              </div>
+              {/*  */}
+              <div className="card w-96 bg-base-100 shadow-xl m-1 ">
+                <figure className="px-10 pt-10">
+                  <Image
+                    src="https://img.freepik.com/free-vector/paper-style-dynamic-lines-background_23-2149008629.jpg"
+                    alt="Shoes"
+                    className="rounded-xl"
+                    width={400}
+                    height={400}
+                  />
+                </figure>
+                <div className="card-body items-center text-center">
+                  <h2 className="card-title">Name</h2>
+                  <p>desc</p>
+                  <div className="card-actions">
+                    <button className="btn btn-primary">Resume</button>
+                  </div>
+                </div>
+              </div>
+              {/*  */}
             </div>
           </div>
         </div>
